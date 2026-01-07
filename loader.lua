20545311db5ce861799547a2fe19e2a802a0b274
incognitocoded <nazarperec18@gmail.com>
2026-01-07 20:33:36 +0200

    Update loader to use new GitHub URL for script

--- loader.lua
+++ loader.lua
@@ -1,12 +1,1 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-
-local success, content = pcall(function()
-    return game:HttpGet(my_github_url)
-end)
-
-if success then
-    loadstring(content)()
-else
-    -- Если ссылка битая или нет интернета, выдаст ошибку в консоль F9
-    warn("LOST HUB: Error connecting to GitHub!")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/incognitocoded/LostHub2/main/main.lua"))()
