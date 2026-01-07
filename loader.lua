3dff0fe68c0f20f78bb83fe661ba06526bb3b57a
incognitocoded <nazarperec18@gmail.com>
2026-01-07 20:31:09 +0200

    Add loader to fetch script from GitHub
    Fetch and execute Lua script from GitHub with error handling.

---
+++ loader.lua
@@ -0,0 +1,12 @@
+local my_github_url = "https://raw.githubusercontent.com/incognitocoded/LostHub2/blob/main/main.lua" 
+
+local success, content = pcall(function()
+    return game:HttpGet(my_github_url)
+end)
+
+if success then
+    loadstring(content)()
+else
+    -- Если ссылка битая или нет интернета, выдаст ошибку в консоль F9
+    warn("LOST HUB: Error connecting to GitHub!")
+end
